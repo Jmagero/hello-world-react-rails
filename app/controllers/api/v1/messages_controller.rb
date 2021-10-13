088698dcd7dc3a6816f9a2a525b57968aefbf215
Jocyline Magero <jmagero2017@gmail.com>
2021-10-13 10:57:59 -0500

    Add controller for random greeting

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -4,8 +4,8 @@ class Api::V1::MessagesController < ApplicationController
     render json: messages, status: 200
   end
 
-  def show
-    message = Message.find(Message.pluck(:id).shuffle.first)
-    render json: message, status: 200
+  def hello
+    @message = Message.find(Message.pluck(:id).sample)
+    render json: @message, status: 200
   end
 end
