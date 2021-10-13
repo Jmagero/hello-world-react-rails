97c7dcc224168e6a0f0723ec218cd4ae2221dbfe
Jocyline Magero <jmagero2017@gmail.com>
2021-10-13 10:58:32 -0500

    Add greeting route

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,24 +1,19 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
-import reducers from '../src/reducers/messages'
 import Greeting from './Greeting'
+import store from '../redux/configureStore'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-
-const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
-
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
+const App = () => (
+  <>
     <Provider store={store}>
       <Router>
-        <Route path="/" component={Greeting}/>
+        <Switch>
+          <Route exact path="/" render={() => <Greeting />} />
+        </Switch>
       </Router>
-    </Provider>,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
+    </Provider>
+  </>
+);
 
+export default App;
